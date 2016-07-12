d0c112cda754dbf88cefdd38462e754b8e065618
Muhummad Patel <muhummad.patel@gmail.com>
2016-07-12 22:11:43 +0200

    Allow Whitelistedusername model to be managed through the admin site.

--- students/admin.py
+++ students/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import WhitelistedUsername
+
+
+class WhitelistedUsernameAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(WhitelistedUsername, WhitelistedUsernameAdmin)
