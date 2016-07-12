49f3c5bf5b95a7d678e541d93e0999f37f8a2b26
Muhummad Patel <muhummad.patel@gmail.com>
2016-07-12 22:21:25 +0200

    Use class decorator instead of floating statement to register WhitelistedUsernameAdmin class.

--- students/admin.py
+++ students/admin.py
@@ -3,8 +3,6 @@ from django.contrib import admin
 from .models import WhitelistedUsername
 
 
+@admin.register(WhitelistedUsername)
 class WhitelistedUsernameAdmin(admin.ModelAdmin):
     pass
-
-
-admin.site.register(WhitelistedUsername, WhitelistedUsernameAdmin)
