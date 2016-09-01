8f59ffb2082dc82d55a2ff5edc6e570059b90f35
Muhummad Patel <muhummad.patel@gmail.com>
2016-09-01 22:25:09 +0200

    Fix 404 error on login redirect by adding trailing slashes to login urls in settings.py.

--- raspied/settings.py
+++ raspied/settings.py
@@ -177,7 +177,7 @@ PIPELINE = {
     },
 }
 
-LOGIN_URL = '/students/accounts/login'
-LOGIN_REDIRECT_URL = '/students/home'
+LOGIN_URL = '/students/accounts/login/'
+LOGIN_REDIRECT_URL = '/students/home/'
 
 STREAMING_SERVER_IP = '105.186.195.110'
