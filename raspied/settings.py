b8c84e0bc6c918c040e25c9bdace1e1f3df4525a
Muhummad Patel <muhummad.patel@gmail.com>
2016-09-01 22:38:46 +0200

    Add basic logging setup to settings.py to help debug server errors.

--- raspied/settings.py
+++ raspied/settings.py
@@ -181,3 +181,32 @@ LOGIN_URL = '/students/accounts/login/'
 LOGIN_REDIRECT_URL = '/students/home/'
 
 STREAMING_SERVER_IP = '105.186.195.110'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'raspied.log',
+            'formatter': 'verbose'
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'propagate': True,
+            'level': 'DEBUG',
+        },
+    }
+}
