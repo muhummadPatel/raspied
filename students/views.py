a153d235c8f5287d651456985ae013b86e8337f5
Muhummad Patel <muhummad.patel@gmail.com>
2016-09-05 19:40:30 +0200

    Allow users to upload their own scripts into the code editor.

--- students/views.py
+++ students/views.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.contrib.auth import authenticate, get_user_model, login
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
 
 from registration import signals
 from registration.backends.simple.views import RegistrationView
@@ -17,8 +18,22 @@ class ExclusiveRegistrationView(RegistrationView):
 
 
 @login_required
+@require_http_methods(['GET', 'POST'])
 def home(request):
-    context = {
-        'streaming_server_ip': getattr(settings, 'STREAMING_SERVER_IP', '105.225.158.228')
-    }
-    return render(request, 'students/home.html', context)
+    context = {}
+    if request.method == 'GET':
+        print 'get view method'
+        context['streaming_server_ip'] = getattr(settings, 'STREAMING_SERVER_IP', '105.225.158.228')
+        return render(request, 'students/home.html', context)
+
+    elif request.method == 'POST':
+        print "post view method"
+        if 'uploaded_file' not in request.FILES:
+            context['user_script'] = "Could not upload file"
+            return render(request, 'students/home.html', context)
+
+        contents = request.FILES['uploaded_file'].read()
+        print contents
+        context['user_script'] = contents
+
+        return render(request, 'students/home.html', context)
