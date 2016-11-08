9ef79bb24d46bbaa6c25e0e98abb1a152970bde1
Muhummad Patel <muhummad.patel@gmail.com>
2016-11-08 12:14:13 +0200

    Document registration form js.

--- static/registration/js/registration_form.js
+++ static/registration/js/registration_form.js
@@ -1,3 +1,9 @@
+/* registration_form.js
+ *
+ * Once the registration form has been rendered, this appends the password help
+ * text to the password field.
+ */
+
 $(function(){
   var passwordHelpText = "<p class=\"help\">The password must contain at least 8 characters and cannot be entirely numeric.</p>";
   $(".input-field > input#id_password1").parent().append(passwordHelpText);
