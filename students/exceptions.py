5c8d7ff51113c6ba8f85f054c83fe3abeb24160f
Muhummad Patel <muhummad.patel@gmail.com>
2016-11-08 12:11:51 +0200

    Clean up and document student module util files.

--- students/exceptions.py
+++ students/exceptions.py
@@ -2,6 +2,10 @@ import json
 
 
 class ClientError(Exception):
+    """
+    Custom Exception class that allows you to send an error message over the
+    specified channel.
+    """
     def __init__(self, code):
         super(ClientError, self).__init__(code)
         self.code = code
--- students/forms.py
+++ students/forms.py
@@ -12,18 +12,24 @@ User = get_user_model()
 
 
 class CustomAuthenticationForm(AuthenticationForm):
+    """
+    Extension of default AuthenticationForm to replace 'username' label with 'student number'.
+    """
     def __init__(self, *args, **kwargs):
         super(CustomAuthenticationForm, self).__init__(*args, **kwargs)
         self.fields['username'].label = 'Student number'
 
 
 class ExclusiveRegistrationForm(RegistrationForm):
+    """
+    Extension of default RegistrationForm to replace 'username' label with
+    'student number' and only allow registration with whitelisted usernames.
+    """
     def __init__(self, *args, **kwargs):
         super(ExclusiveRegistrationForm, self).__init__(*args, **kwargs)
         self.fields['username'].label = 'Student number'
 
     def clean(self):
-        # TODO: try catch KeyError here to avoid empty form error
         form_username = self.cleaned_data['username']
         try:
             # If this runs without raising an exception, then the username is in
