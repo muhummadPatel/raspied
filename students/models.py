a7a24279aa1377a7757682bf58b4e70e85911391
Muhummad Patel <muhummad.patel@gmail.com>
2016-11-08 12:12:59 +0200

    Clean up and document student module models.

--- students/models.py
+++ students/models.py
@@ -11,8 +11,11 @@ from StringIO import StringIO
 
 
 class WhitelistedUsername(models.Model):
-    # TODO: change this username field to only allow usernames matching the UCT
-    # student number regex
+    """
+    Whitelisted usernames are the usernames/student numbers that have been
+    authorised to register on the RASPIED site. Any registration attempt with a
+    non WhitelistedUsername will fail.
+    """
     username = models.CharField(max_length=50)
 
     def __str__(self):
@@ -24,6 +27,11 @@ class WhitelistedUsername(models.Model):
 
 
 class Booking(models.Model):
+    """
+    These are the robot bookings. Helps keep track of which student
+    is using the robot at any given time and manage shared access to the robot
+    without any conflicts, etc.
+    """
     user = models.ForeignKey(User, editable=False, default=None, blank=False)
     start_time = models.DateTimeField(blank=False)
     end_time = models.DateTimeField(blank=False)
@@ -33,7 +41,10 @@ class Booking(models.Model):
 
 
 class RobotTerminal(models.Model):
-
+    """
+    Models the connection with the robot. Also handles sending and running code,
+    and halting code executions.
+    """
     title = models.CharField(max_length=255)
 
     def __str__(self):
@@ -52,6 +63,10 @@ class RobotTerminal(models.Model):
         return 0
 
     def sanitize_code_lines(self, lines):
+        """
+        Used to clean up the submitted code and format it to allow running it in
+        the python shell. Makes sure the indents are handled properly.
+        """
         out = []
         for i in range(len(lines)):
             next_line = ''
@@ -70,6 +85,10 @@ class RobotTerminal(models.Model):
         return out
 
     def send_halt(self):
+        """
+        Halts any currently executing Python processes on the Robot and recovers
+        and resets the robot to its original state.
+        """
         try:
             output_message = 'Attempting to kill the code...\n'
             fb = {'robot': str(self.id), 'message': output_message}
@@ -107,6 +126,11 @@ class RobotTerminal(models.Model):
             print(e)
 
     def send_command(self, command, user):
+        """
+        Sends the submitted script(command) to the robot and runs it one line at
+        a time.
+        """
+        # split the code into lines and sanitize the lines
         lines = StringIO(command).readlines()
         lines[-1] += '\n'
         lines += ['\n\n\n']
@@ -120,6 +144,7 @@ class RobotTerminal(models.Model):
         )
 
         try:
+            # open ssh session to the robot
             ssh = pxssh.pxssh()
             hostname = getattr(settings, 'ROBOT_HOSTNAME')
             username = getattr(settings, 'ROBOT_USERNAME')
@@ -129,10 +154,12 @@ class RobotTerminal(models.Model):
             ssh.sendline('cd HonoursProject')
             ssh.prompt()
 
+            # from now on, listen for the python prompt
             ssh.PROMPT = '>{3}|\.{3}'
             ssh.sendline('python')
             ssh.prompt()
 
+            # send each line to the robot and report output to the client
             for line in lines:
                 ssh.send(line)
                 ssh.prompt()
@@ -142,17 +169,18 @@ class RobotTerminal(models.Model):
                     {'text': json.dumps(fb)}
                 )
 
-            # NOTE: the code is as yet untested!!!
             output_message = 'Code execution complete. Now resetting the robot...'
             fb = {'robot': str(self.id), 'message': output_message, 'code_done': True}
             self.websocket_group.send(
                 {'text': json.dumps(fb)}
             )
 
+            # send cleanup lines to reset the robot to its starting position
             for line in cleanup_lines:
                 ssh.send(line)
                 ssh.prompt()
 
+            #  exit python shell and close ssh connection
             ssh.PROMPT = '$'
             ssh.sendline('quit()')
             ssh.prompt()
